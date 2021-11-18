07647f0053e408aeb94d396d3e5c004a23f5aafe
rishikumar-p <53998053+rishikumar-p@users.noreply.github.com>
2021-11-18 17:26:47 +0530

    Update Main.java

--- Java/src/Assignment5/assignment/main/Main.java
+++ Java/src/Assignment5/assignment/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
         TempClass1 tempClass1 = new TempClass1();
         tempClass1.printVariables();
-//        tempClass1.printLocalVaribles(); Doesnt work as it uses uninitialized variables
+//        tempClass1.printLocalVaribles(); Doesnt work as it uses uninitialized local variables
 
         TempClass2 tempClass2 = TempClass2.initializeMember("Hello World");
         tempClass2.printMember();
